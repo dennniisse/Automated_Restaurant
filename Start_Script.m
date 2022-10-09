be36164b9ca0169a0d96c77a5bd44f42ee85d106
dennniisse <fernandezdenisse0605@gmail.com>
2022-10-09 23:13:06 +1100

    Determining tray location, adding tray code

--- Start_Script.m
+++ Start_Script.m
@@ -1,5 +1,14 @@
+clear; clc; clf;
 figure;
 hold on;
 dobot = GetDobot();
 UR3 = GetUR3();
 
+offset = -0.67; %adjusting the height of environment so UR3 is placed on top of the table
+[f,v,data] = plyread('tray.ply','tri');
+vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+% Plot the environment, account for any changes in the UR3's base
+trisurf(f,v(:,1) + 0.75...
+    , v(:,2) + 0.4 ...
+    , v(:,3) ...
+    ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
