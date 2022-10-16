18dc00dd7c20ca5d69c06631afee7c9c7b896efa
Denisse Fernandez <90050059+dennniisse@users.noreply.github.com>
2022-10-16 22:22:23 +1100

    Add files via upload
    Maarrrsssssssss environment

---
+++ Mars_environment/imageUpload.m
@@ -0,0 +1,10 @@
+clf;
+% top left, top right ; bottom right, bottom left
+surf([-8,-8;8,8],[-8,8;-8,8],[0,0;0,0],'CData',imread('ground_mars.jpg'),'FaceColor','texturemap');
+hold on;
+surf([8,-8;8,-8],[8,8;8,8],[5,5;0,0],'CData',imread('wall_mars.jpg'),'FaceColor','texturemap');
+
+surf([8,8;8,8],[8,-8;8,-8],[5,5;0,0],'CData',imread('wall_mars_1.jpg'),'FaceColor','texturemap');
+axis on;
+
+
