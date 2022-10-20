3d025de94f50ccd396f053dc41a967c0399a5091
KennyTafianoto <Kennytafianoto@gmail.com>
2022-10-21 01:38:10 +1100

    GUI Testing

--- Start_Script.m
+++ Start_Script.m
@@ -4,6 +4,13 @@ hold on;
 UR3 = GetUR3();
 
 % Test to move UR3 to ingredients
-goal = [-1 0 0];
-h = PlaceObject("rock.ply",goal);
-UR3.move(goal, false);
+% goal = [-1 0 0];
+% h = PlaceObject("rock.ply",goal);
+% UR3.move(goal, false);
+
+GUIAPP = GUI;
+GUIAPP.getRobot(UR3)
+
+while(1)
+    controlm = GUIAPP.read;
+end
