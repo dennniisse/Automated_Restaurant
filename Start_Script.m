e82c31d476c1a3df08bb2cafc8c3a1d6e4d8477e
Denisse Fernandez <fernandezdenisse0605@gmail.com>
2022-10-21 14:35:39 +1100

    edits_realism

--- Start_Script.m
+++ Start_Script.m
@@ -6,8 +6,8 @@ UR3 = GetUR3();
 % Test to move UR3 to ingredients
 goal = [-1 0 0];
 h = PlaceObject("rock.ply",goal);
-UR3.move(goal, false);
-goal = [-0.112 0.539 0.298];
-UR3.initDropOff();
-UR3.move(goal,false);
+% UR3.move(goal, false);
+% goal = [-0.112 0.539 0.298];
+% UR3.initDropOff();
+% UR3.move(goal,false);
 
