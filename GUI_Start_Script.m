9deac4537138391c1bcabcd1675393d25f847be7
KennyTafianoto <Kennytafianoto@gmail.com>
2022-10-21 17:01:50 +1100

    GUI Updated
    -added E-Stop framework
    -added roll pitch yaw jogging
    -changed to DLS joint calculation

--- GUI_Start_Script.m
+++ GUI_Start_Script.m
@@ -1,13 +1,7 @@
 clear; clc; clf;
 hold on;
 axis equal;
-% dobot = GetDobot();
 UR3 = GetUR3();
-
-% Test to move UR3 to ingredients
-% goal = [-1 0 0];
-% h = PlaceObject("rock.ply",goal);
-% UR3.move(goal, false);
 
 GUIAPP = GUI;
 GUIAPP.getRobot(UR3)
